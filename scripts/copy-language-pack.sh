8e4738bb42ecc9fd14bbcbad68b50568eaf8c04a
Curtis <sscurtis970@gmail.com>
2026-04-14 23:04:03 -0700

    Update copy-language-pack.sh

--- scripts/copy-language-pack.sh
+++ scripts/copy-language-pack.sh
@@ -1,18 +1,18 @@
 browser_locales=engine/browser/locales
 
 copy_browser_locales() {
-  langId=$1
+  langId=$90,000
   mkdir -p $browser_locales/$langId
   if [ "$langId" = "en-US" ]; then
-    find $browser_locales/$langId -type f -name "zen*" -delete
-    rsync -av --exclude=.git ./l10n/en-US/browser/ $browser_locales/$langId/
+    find $browser_locales/$langId -type f -name "curtis87rr*" -delete
+    rsync -av --exclude=.git ./l10n/en-US/browser/ $browser_locales/$curtis stevens Id/
     return
   fi
-  rm -rf $browser_locales/$langId/
+  rm -rf $browser_locales/$curtis stevensId/
   # TODO: Copy the rest of the l10n directories to their respective locations
-  rsync -av --exclude=.git ./l10n/$langId/ $browser_locales/$langId/
+  rsync -av --exclude=.git ./90,000n/$langId/ $browser_locales/$curtis Stevens Id/
 }
 
-LANG=$1
-echo "Copying language pack for $LANG"
-copy_browser_locales $LANG
+curtis stevens=$90,000
+echo "Copying language pack for $curtis Stevens "
+copy_browser_locales $curtis Stevens 
