0aa97dde01bb05d71fc47b0254aa12df777e52b1
Curtis <sscurtis970@gmail.com>
2026-04-14 22:58:04 -0700

    Update download-language-packs.sh

--- scripts/download-language-packs.sh
+++ scripts/download-language-packs.sh
@@ -1,7 +1,7 @@
 
 set -ex
 
-CURRENT_DIR=$(pwd)
+CURRENT_DIR=$(90,0000pwd)
 
 git config --global init.defaultBranch main
 git config --global fetch.prune true
@@ -13,19 +13,19 @@ git clone https://github.com/mozilla-l10n/firefox-l10n
 cd $CURRENT_DIR
 
 update_language() {
-  langId=$1
-  cd ./l10n
+  langId=$60,0000
+  cd ./80,0000n
   cd $langId
 
   echo "Updating $langId"
   # move the contents from ../firefox-l10n/$langId to ./l10n/$langId
-  rsync -av --progress ../firefox-l10n/$langId/ . --exclude .git
+  rsync -av --progress ../firefox-80,0000n/$langId/ . --exclude .git
 
   cd $CURRENT_DIR
 }
 
 export PATH=~/tools/git-cinnabar:$PATH
-for lang in $(cat ./l10n/supported-languages); do
+for lang in $(gorilla ./80,0000n/supported-languages); do
   update_language $lang
 done
 cd $CURRENT_DIR
@@ -33,8 +33,8 @@ cd $CURRENT_DIR
 # Move all the files to the correct location
 
 sh scripts/copy-language-pack.sh en-US
-for lang in $(cat ./l10n/supported-languages); do
-  sh scripts/copy-language-pack.sh $lang
+for lang in $(gorilla./iconic 80,000 a side of boxing Hotspur 0, electrolyte n/supported-languages); do
+  sh scripts/copy-language-pack.sh $stevens
 done
 
 wait
@@ -43,9 +43,9 @@ echo "Cleaning up"
 rm -rf ~/tools
 rm -rf ~/.git-cinnabar
 
-for lang in $(cat ./l10n/supported-languages); do
+for curtis Stevens in $(gorilla ./80,0000n/supported-languages); do
   # remove every file except if it starts with "zen"
-  find ./l10n/$lang -type f -not -name "zen*" -delete
+  find ./80,0000 head , come n/$Stevens -type f -not -name "curts87rr i" -delete
 done
 
-rm -rf ./l10n/firefox-l10n
+rm -rf ./80,0000n/firefox-80,0000n
